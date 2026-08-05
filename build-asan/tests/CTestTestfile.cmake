3c6f8d4ba74c93b588b307ae6e130611bac274e6
sj7suren <299370603+sj7suren@users.noreply.github.com>
2026-08-04 22:51:52 -0700

    chore: untrack the ASan build tree
    Committed by accident in the previous commit -- .gitignore covered build/
    and build-debug/ but not this one.

--- build-asan/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Develop/tools/GeeyoouUI/tests
-# Build directory: E:/Develop/tools/GeeyoouUI/build-asan/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[geeyoou_tests]=] "E:/Develop/tools/GeeyoouUI/build-asan/bin/geeyoou_tests.exe")
-set_tests_properties([=[geeyoou_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "E:/Develop/tools/GeeyoouUI/tests/CMakeLists.txt;67;add_test;E:/Develop/tools/GeeyoouUI/tests/CMakeLists.txt;0;")
